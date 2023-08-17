b0430698f1f745f5304dee84961edce0ce4644be
Manan Sharma <mansharm71@gmail.com>
2023-08-16 23:39:11 -0400

    created connection between frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors module
 const app = express();
 
+app.use(cors()); // Enable CORS for all routes
+
 app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+  res.send('Byefsafas f tn');
+});
+
+// Sample expenses data (replace this with your actual data)
+const expenses = [
+  { id: 1, name: 'Groceries', amount: 50 },
+  { id: 2, name: 'Utilities', amount: 100 },
+  // ... other expenses
+];
+
+// Define an endpoint to fetch expenses
+app.get('/api/expenses', (req, res) => {
+  // You can replace this with logic to fetch expenses from a database or other data source
+  res.json(expenses);
 });
 
 const PORT = process.env.PORT || 5000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([]); // Corrected variable name
 
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/expenses')
+      .then(response => {
+        setExpenses(response.data); // Corrected function name
+      })
+      .catch(error => {
+        console.error('Error fetching expenses:', error);
+      });
+  }, []);
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Expense List</h1>
+      <ul>
+        {expenses.map(expense => (
+          <li key={expense.id}>{expense.name} - ${expense.amount}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
