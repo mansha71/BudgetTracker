aac556a935c5a51d34843db25eb040bcf741e69a
Manan Sharma <mansharm71@gmail.com>
2023-08-17 23:20:34 -0400

    woprking on databse

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,61 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors module
+const cors = require('cors');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
+app.use(express.json()); // Parse JSON request bodies
 
-app.get('/', (req, res) => {
-  res.send('Byefsafas f tn');
+// Create a connection to the database file (e.g., "mydatabase.db")
+const db = new sqlite3.Database('mydatabase.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error('Error opening database', err.message);
+    } else {
+        console.log('Connected to the database');
+        // Create table if it doesn't exist
+        db.run(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                name TEXT
+            )
+        `);
+    }
 });
 
-// Sample expenses data (replace this with your actual data)
-const expenses = [
-  { id: 1, name: 'Groceries', amount: 50 },
-  { id: 2, name: 'Utilities', amount: 100 },
-  // ... other expenses
-];
-
-// Define an endpoint to fetch expenses
-app.get('/api/expenses', (req, res) => {
-  // You can replace this with logic to fetch expenses from a database or other data source
-  res.json(expenses);
+// Store name in the database
+app.post('/api/storeName', (req, res) => {
+    const name = req.body.name;
+    const query = 'INSERT INTO users (name) VALUES (?)';
+    db.run(query, [name], (err) => {
+        if (err) {
+            console.error('Error storing name', err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json({ message: 'Name stored successfully' });
+        }
+    });
 });
+
+// Retrieve the latest stored name from the database
+app.get('/api/retrieveName', (req, res) => {
+    const query = 'SELECT name FROM users ORDER BY id DESC LIMIT 1';
+    db.get(query, (err, row) => {
+        if (err) {
+            console.error('Error retrieving name', err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            if (row) {
+                res.json({ name: row.name });
+            } else {
+                res.json({ name: 'Name not found' });
+            }
+        }
+    });
+});
+
+// ...
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,43 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-import axios from 'axios'
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [expenses, setExpenses] = useState([]); // Corrected variable name
+  const [expenses, setExpenses] = useState([]);
+  const [name, setName] = useState('');
+  const [storedName, setStoredName] = useState('');
 
   useEffect(() => {
+    // Fetch expenses
     axios.get('http://localhost:5000/api/expenses')
       .then(response => {
-        setExpenses(response.data); // Corrected function name
+        setExpenses(response.data);
       })
       .catch(error => {
         console.error('Error fetching expenses:', error);
       });
+
+    // Retrieve stored name
+    axios.get('http://localhost:5000/api/retrieveName')
+      .then(response => {
+        setStoredName(response.data.name);
+      })
+      .catch(error => {
+        console.error('Error retrieving name:', error);
+      });
   }, []);
-  
+
+  const handleNameSubmit = () => {
+    // Store name
+    axios.post('http://localhost:5000/api/storeName', { name })
+      .then(response => {
+        console.log(response.data.message);
+        setName(''); // Clear input after successful submission
+      })
+      .catch(error => {
+        console.error('Error storing name:', error);
+      });
+  };
+
   return (
     <div>
       <h1>Expense List</h1>
@@ -26,6 +46,18 @@ function App() {
           <li key={expense.id}>{expense.name} - ${expense.amount}</li>
         ))}
       </ul>
+
+      <h2>Store and Retrieve Name</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          placeholder="Enter your name"
+        />
+        <button onClick={handleNameSubmit}>Store Name</button>
+      </div>
+      {storedName && <p>Stored name: {storedName}</p>}
     </div>
   );
 }
