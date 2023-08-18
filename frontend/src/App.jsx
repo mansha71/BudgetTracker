2cafe689f0b9389f487aa564bc37bdd0fe60709e
Manan Sharma <mansharm71@gmail.com>
2023-08-17 23:08:14 -0400

    trying to add databse

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import axios from 'axios'
 
 
